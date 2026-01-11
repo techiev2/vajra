76642635826ece6ac2fc9764c3ad140a1b45d31c
Sriram Velamur <techiev2@users.noreply.github.com>
2026-01-12 01:42:26 +0530

    Release - v1.5.2 (#11)
    Switches to createServer from https module.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { createServer } from 'node:http'
+import { createServer } from 'node:https'
 import { readFile, access } from 'node:fs/promises'
 import { createReadStream, existsSync } from 'node:fs'
 import { resolve } from 'path'
